37bcc5e7d6ff137e77ba5fefb91c08f897abcae3
bibis <bertha@gmail.com>
2025-04-09 13:15:37 +0200

    modificaciones clase 6

--- Angular-IV/VeladaAngular/src/app/servicios/data.service.ts
+++ Angular-IV/VeladaAngular/src/app/servicios/data.service.ts
@@ -1,16 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Player } from '../common/player';
+import { map, Observable } from 'rxjs';
+import { Participante, Player } from '../common/player';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private urlData = 'data/data.json'
   constructor( private http: HttpClient) { }
 
   getPlayers(): Observable<Player>{
-    return this.http.get<Player>('data/data.json')
+    return this.http.get<Player>(this.urlData)
   }
+
+  public getparticipanteyId(id: string): Observable<Participante> {
+    return this.getPlayers()
+      .pipe(map(players => {
+        const participante = players.participantes
+          .find(participante => participante.alias === id);
+        if (!participante) {
+          throw new Error('No se encontró el jugador con el id: ' + id);
+        }
+        return participante;
+      }));
+  }
+
 }
